2b3daea9ef55879909a613e456f454871ea6225d
Cardoso <32268902+Cardoso31@users.noreply.github.com>
2019-08-24 23:17:35 -0300

    Add files via upload

---
+++ Calculadora_de-ip/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require "views/cabecalho.php";
+require "models/class.php";
+
+
+	if (isset($infos)) {
+
+		require "views/informations.php";
+	//	echo "<pre> ";
+	//	print_r($infos);
+
+	}
+
+
+   
